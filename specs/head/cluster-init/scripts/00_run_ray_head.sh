97b67302f36e73cefb3ab4d1688b215d66cf24ef
Yuval Mazor <yuvmaz@gmail.com>
2025-05-12 10:04:27 +0300

    Make ports configurable

--- specs/head/cluster-init/scripts/00_run_ray_head.sh
+++ specs/head/cluster-init/scripts/00_run_ray_head.sh
@@ -4,8 +4,10 @@ set -e
 
 
 cluster_user=$(jetpack config cyclecloud.cluster.user.name)
+ray_port=$(jetpack config ray.port)
+ray_client_server_port=$(jetpack config ray.client_server_port)
 
 
 sudo -u $cluster_user ray stop
-sudo -u $cluster_user ray start --disable-usage-stats --head --port 6379 --ray-client-server-port=10001
+sudo -u $cluster_user ray start --disable-usage-stats --head --port=$ray_port --ray-client-server-port=$ray_client_server_port
 
--- specs/worker/cluster-init/scripts/00_run_ray.worker.sh
+++ specs/worker/cluster-init/scripts/00_run_ray.worker.sh
@@ -5,7 +5,8 @@ set -e
 
 cluster_user=$(jetpack config cyclecloud.cluster.user.name)
 cluster_name=$(jetpack config cyclecloud.cluster.name)
+ray_port=$(jetpack config ray.port)
 
 
 sudo -u $cluster_user ray stop
-sudo -u $cluster_user ray start --disable-usage-stats --address="$cluster_name-head:6379"
+sudo -u $cluster_user ray start --disable-usage-stats --address="$cluster_name-head:$ray_port"
